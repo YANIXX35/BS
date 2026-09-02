9e04fa4cac7de3505984262ad046a6d3cb072f5c
Y-kyliane45 <yanisse.kouassi-yao@epitech.eu>
2026-09-02 07:18:28 -0700

    fix: use custom domain api.notifymails.com for backend URL

--- angular/mail-dashboard/src/environments/environment.ts
+++ angular/mail-dashboard/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://backend-mail-knt0.onrender.com',
+  apiUrl: 'https://api.notifymails.com',
   firebase: {
     apiKey: 'AIzaSyDa3yXuaplIOg1d105B6ZU961K0FwbjVEc',
     authDomain: 'mailnotifier-d92d1.firebaseapp.com',
